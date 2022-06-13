8d0a55c8ee88dceee2ee98b19fc82565b87c9e10
Matheus Melo Antiquera <matheusantiquera@finchsolucoes.com.br>
2022-06-13 20:04:03 -0300

    Strategy
    https://refactoring.guru/pt-br/design-patterns/strategy

---
+++ src/CalculadoraDeImpostos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Alura\DesignPattern;
+
+use Alura\DesignPattern\Impostos\Imposto;
+
+class CalculadoraDeImpostos
+{
+    public function calcular(Orcamento $orcamento, Imposto $imposto)
+    {
+        return $imposto->calculaImposto($orcamento);
+    }
+}
