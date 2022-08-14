5bbe4174adc59b7b5cbf72696b30483a7498f723
Matheus Melo Antiquera <matheusantiquera@finchsolucoes.com.br>
2022-08-14 19:04:05 -0300

    State - Problema Inicial
    Muitos ifs e o estado é uma string o que não é muito recomendável usar

--- src/Orcamento.php
+++ src/Orcamento.php
@@ -6,4 +6,20 @@ class Orcamento
 {
     public int $qtdItens;
     public float $valor;
+    private $estadoAtual;
+
+    public function calculaDescontoExtra(): float
+    {
+        if ($this->estadoAtual == "EM_APROVACAO") {
+            return $this->valor * 0.05;
+        }
+
+        if ($this->estadoAtual == "APROVADO") {
+            return $this->valor * 0.02;
+        }
+
+        throw new \DomainException(
+            "Orçamentos reprovados e finalizados não podem receber descontos"
+        );
+    }
 }
